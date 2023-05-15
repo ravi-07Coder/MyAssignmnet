a532a89dedadebffe841baeac41559f0f1943613
ravi-07Coder <75900722+ravi-07Coder@users.noreply.github.com>
2023-05-15 23:58:13 +0530

    Add files via upload

---
+++ stringbuilder/QuestionTwo.java
@@ -0,0 +1,9 @@
+package stringbuilder;
+
+public class QuestionTwo {
+    public static void main(String[] args) {
+        String s="hello";
+        String d=s.concat(" How are you");
+        System.out.println(d);
+    }
+}
