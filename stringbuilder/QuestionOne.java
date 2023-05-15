24d030c412c7a924d2d7bded76e63655b605e20d
ravi-07Coder <75900722+ravi-07Coder@users.noreply.github.com>
2023-05-15 23:56:31 +0530

    Add files via upload

---
+++ stringbuilder/QuestionOne.java
@@ -0,0 +1,9 @@
+package stringbuilder;
+
+public class QuestionOne {
+    public static void main(String[] args) {
+       String  s="hello world";
+       int k=s.length();
+        System.out.println(k);
+    }
+}
